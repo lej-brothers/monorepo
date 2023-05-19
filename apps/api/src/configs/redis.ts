ee94836110e2aa5b674b4f4a2b995e67feddf494
Garfdev <garfdev.13@gmail.com>
2023-05-19 21:51:21 +0700

    feat: add docker compose

--- apps/api/src/configs/redis.ts
+++ apps/api/src/configs/redis.ts
@@ -1,7 +1,7 @@
 import { RedisClientType, createClient } from "redis";
 import { REDIS_URL } from "./secrets";
 
-const REDIS_CLIENT: RedisClientType = createClient({ legacyMode: true, url: REDIS_URL });
+const REDIS_CLIENT: RedisClientType = createClient({ url: REDIS_URL });
 REDIS_CLIENT.connect().catch(console.error)
 
 export default REDIS_CLIENT;
