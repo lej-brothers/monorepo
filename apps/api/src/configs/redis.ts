9a46702fb93492240545fbdf486f6e408413bb5a
Garfdev <garfdev.13@gmail.com>
2023-05-20 12:21:49 +0700

    fix: add exist singal when redis cant connect

--- apps/api/src/configs/redis.ts
+++ apps/api/src/configs/redis.ts
@@ -2,6 +2,9 @@ import { RedisClientType, createClient } from "redis";
 import { REDIS_URL } from "./secrets";
 
 const REDIS_CLIENT: RedisClientType = createClient({ url: REDIS_URL });
-REDIS_CLIENT.connect().catch(console.error)
+REDIS_CLIENT.connect().catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
 
 export default REDIS_CLIENT;
