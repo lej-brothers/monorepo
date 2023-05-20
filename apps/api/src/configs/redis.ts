dd8234863263171ace7577e8b1e4d51d762d90e3
Garfdev <garfdev.13@gmail.com>
2023-05-20 12:47:11 +0700

    fix: update redis

--- apps/api/src/configs/redis.ts
+++ apps/api/src/configs/redis.ts
@@ -1,7 +1,9 @@
 import { RedisClientType, createClient } from "redis";
 import { REDIS_URL } from "./secrets";
 
-const REDIS_CLIENT: RedisClientType = createClient({ url: REDIS_URL });
+const REDIS_CLIENT: RedisClientType = createClient({ legacyMode: true, url: REDIS_URL, socket: {
+    connectTimeout: 60000
+  } });
 REDIS_CLIENT.connect().catch((error) => {
   console.error(error)
   process.exit(1)
