9c30a54f632e269c88939f12e44b3e29ad07962b
Garfdev <garfdev.13@gmail.com>
2023-05-23 15:09:52 +0700

    feat update web

--- apps/web/components/Navbar/components/CartDropdown/constants.tsx
+++ apps/web/components/Navbar/components/CartDropdown/constants.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export const ITEMS: MenuProps["items"] = [
   {
     key: "1",
-    label: <Link className='flex items-center' rev="123" href="/orders"><BorderlessTableOutlined  className='mr-1'/>
+    label: <Link className='flex items-center' href="/orders"><BorderlessTableOutlined  className='mr-1'/>
     Orders</Link>,
   },
 ];
