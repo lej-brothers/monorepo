27c6c2da97488e0909af6c71d1cdf5762c525e06
Garfdev <garfdev.13@gmail.com>
2023-05-24 16:48:50 +0700

    feat: allow digital ocean

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
   images: {
-    domains: ["127.0.0.1"],
+    domains: ["127.0.0.1", "lej-marketplace.sgp1.digitaloceanspaces.com"],
   },
 };
